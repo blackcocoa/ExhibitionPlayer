03e4ef2cabde52f699fa2a622b0ae5616613581b
blackcocoa <blackcocoa@users.noreply.github.com>
2021-04-26 13:36:39 +0900

    ts-loader

---
+++ web/next.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    cssModules: true,
+    // distDir: '../../dist/functions/next',
+    env: {
+        FIREBASE_PROJECT_ID: 'soundscraper-d0273',
+        FIREBASE_APP_ID: '1:749064670057:web:de74f3411864b9695f4bf8',
+        FIREBASE_API_KEY: 'AIzaSyDav5vffiV6v9JovbeBp7LOpPbNggkJvv0',
+        SOUNDCLOUD_CLIENT_ID: 'bzhrcLdRzbS6QiVRq8EvMSoMM6p1KhL5',
+        DEFAULT_AUDITION_DURATION: 20,
+        CIRCLE_DISPLAY_LIMIT: 198,
+        CIRCLE_FETCH_LIMIT: 12,
+    },
+    future: {
+        webpack5: true,
+    },
+    webpack: (config, options) => {
+        config.module.rules.push({
+            test: /\.ts$/,
+            use: [
+                options.defaultLoaders.babel,
+                {
+                    loader: 'ts-loader',
+                },
+            ],
+        })
+
+        return config
+    },
+}
